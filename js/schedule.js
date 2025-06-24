e584ded4264f44341f562b6708a89930e1f51844
Agboke-Adunola <agbokeadunola@gmail.com>
2025-06-24 16:50:48 +0100

    add payment status. #1

---
+++ js/schedule.js
@@ -0,0 +1,7 @@
+const successButton = document.getElementById("successButton");
+// this should come from other person work
+
+// clicking on this should navigate to payment method
+successButton.addEventListener("click", function() {
+  window.location.href = "payment-method.html";
+});
